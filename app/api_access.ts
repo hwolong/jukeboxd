bbb9b2a7991705692a332119deddb947b83ee289
hwolong <130803554+hwolong@users.noreply.github.com>
2025-11-08 17:50:00 -0600

    mandated cover art for primaryRelease

--- app/api_access.ts
+++ app/api_access.ts
@@ -54,7 +54,7 @@ export async function getInfo({ mbid }: { mbid: string }) {
         const releaseGroup = await mbApi.lookup('release-group', mbid);
         const releases = await mbApi.browse('release', { 'release-group': mbid });
         const artist = await mbApi.browse('artist', { 'release-group': mbid });
-        const primaryRelease = releases.releases.find(r => r.date == releaseGroup["first-release-date"]) || releases.releases[0];
+        const primaryRelease = releases.releases.find(r => (r.date == releaseGroup["first-release-date"]) && r["cover-art-archive"].front) || releases.releases[0];
         const coverArt = await caaApi.getReleaseCovers(primaryRelease.id);
         return { releaseGroup, releases, primaryRelease, artist, coverArt };
     } catch (error) {

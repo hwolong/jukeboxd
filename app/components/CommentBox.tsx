2f82b1b62ae39c66bc8e0536fccd7d47c0937bb1
hwolong <130803554+hwolong@users.noreply.github.com>
2025-11-09 00:02:17 -0600

    finished commenting stuff

--- app/components/CommentBox.tsx
+++ app/components/CommentBox.tsx
@@ -7,16 +7,16 @@ if (!supabaseKey) {
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
-function submitComment(mbid: string, event: React.FormEvent<HTMLFormElement>) {
+async function submitComment(mbid: string, event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const comment = formData.get("comment");
     const rating = formData.get("rating");
     console.log(`Submitting comment for MBID ${mbid}: ${comment} with rating ${rating}`);
-    const { data, error } = supabase
+    const { data, error } = await supabase
         .from('Reviews')
         .insert([
-            { mbid: mbid, stars: rating, review: comment},
+            { mbid: mbid, stars: rating, review: comment as string},
         ]);
     if (error) {
         console.error('Error submitting comment:', error);
@@ -43,7 +43,7 @@ export default function CommentBox({mbid}: {mbid: string}) {
                     <option value="10">★★★★★</option>
                 </select>
                 <label htmlFor="comment" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your comment</label>
-                <textarea id="comment" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                <textarea name="comment" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
                 <button type="submit" className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300">Submit</button>
             </form>
         </div>

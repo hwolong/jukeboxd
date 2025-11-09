eba81525129de0890effc30d7ff58750aed720d8
addiebart <addiebart@ku.edu>
2025-11-09 03:45:43 -0600

    review box

--- app/components/Review.tsx
+++ app/components/Review.tsx
@@ -1,6 +1,26 @@
-export default function Review() {
-    
+interface ReviewProps {
+    albumName: string;
+    artistName: string;
+    reviewBody: string;
+    id: string;
+}
+
+export default function Review({albumName, artistName, reviewBody, id}: ReviewProps) {
     return (
-        
+        <div className="bg-(--color-off-white) rounded-3xl p-6 w-full shadow-lg flex flex-row">
+            <div>
+                <div className="flex flex-row items-end h-lh">
+                        <h3 className="text-lg font-bold text-(--color-dark) h-fit">{albumName}</h3>
+                        <p className="text-sm text-(--color-main) font-semibold ml-4 h-full">{artistName}</p>
+                </div>
+                <p className="text-(--color-dark) text-sm leading-relaxed italic">{`"${reviewBody}"`}</p>
+            </div>
+            <div className="grow"></div>
+            <div className="flex flex-col items-center ml-4">
+                <div className="grow"></div>
+                <a href={`/album/${id}`} className="text-lg text-(--color-main) font-semibold whitespace-nowrap">View Album</a>
+                <div className="grow"></div>
+            </div>
+        </div>
     );
 }
--- app/components/ReviewTimeline.tsx
+++ app/components/ReviewTimeline.tsx
@@ -1,10 +1,18 @@
+import Review from "./Review";
+
 export default async function ReviewTimeline() {
     
 
     return (
-        <div className="flex flex-row grow m-8">
+        <div className="flex flex-row grow mx-8 mb-8">
             <div className="grow"></div>
-            <section id="new_reviews" className="flex flex-row items-start w-9/10 md:w-4/5">
+            <section id="new_reviews" className="flex flex-col items-start w-9/10 md:w-4/5">
+                <Review 
+                    albumName="The Dark Side of the Moon" 
+                    artistName="Pink Floyd" 
+                    reviewBody="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+                    id="bang"
+                />
             </section>
             <div className="grow"></div>
         </div>

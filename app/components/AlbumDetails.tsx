7af03be1862a38b4f42733378ddb16214785d5c2
hwolong <130803554+hwolong@users.noreply.github.com>
2025-11-08 23:37:04 -0600

    work on commenting

--- app/components/AlbumDetails.tsx
+++ app/components/AlbumDetails.tsx
@@ -1,4 +1,5 @@
 import { ApiInfo } from '../api_access';
+import CommentBox from './CommentBox';
 
 interface albumDetailsProps {
     apiInfo: ApiInfo
@@ -17,19 +18,12 @@ function sortArtists(artists: Array<{ name: string }>){
 
 export default function AlbumDetails( {apiInfo} : albumDetailsProps) {
     return (
-        <div className="flex flex-col items-start gap-4 p-4">
-            <div className="w-204 h-104 rounded-s">
-                <img 
-                    className="w-full h-full object-contain"
-                    src={apiInfo.coverArt.images[0].image} 
-                    alt={`${apiInfo.releaseGroup.title} cover art`} 
-                />
-            </div>
-            <div className="w-204 text-center">
-                <h2 className="text-m mb-2">Artist: {sortArtists(apiInfo.artist.artists)}</h2>
-                <h3 className="text-xl font-bold italic mb-2">{apiInfo.releaseGroup.title}</h3>
-                <h3 className="text-lg">Released {apiInfo.releaseGroup['first-release-date']}</h3>
-            </div>
+        <div className="flex flex-col items-center justify-center gap-4 border">
+            <img className="rounded-lg pt-4 pr-2 pl-2 max-w-[50%]" src={apiInfo.coverArt.images[0].image} alt={`${apiInfo.releaseGroup.title} cover art`} />
+            <h1 className="text-xl font-bold">{sortArtists(apiInfo.artist.artists)}</h1>
+            <h3 className="text-xl font-bold italic">{apiInfo.releaseGroup.title}</h3>
+            <h3 className="text-lg">Released {apiInfo.releaseGroup['first-release-date']}</h3>
+            <CommentBox mbid={apiInfo.releaseGroup.id} />
         </div>
     )
 

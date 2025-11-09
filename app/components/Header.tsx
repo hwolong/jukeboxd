ea285571e9a5cde0b4d47a760cc5ec547df828a5
hwolong <130803554+hwolong@users.noreply.github.com>
2025-11-09 05:00:42 -0600

    finished search functions

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,13 +14,18 @@ export default function Header() {
 
     return (
         <header className="h-12 w-full bg-(--color-main) flex items-center p-2">
-            <nav className="flex w-full items-center">
+            <div className="flex-grow">
+                <a className="text-lg font-bold text-white" href="/">Jukeboxd</a>
+            </div>
+            <div className="flex-grow">
+            <nav className="flex w-full items-center block">
                 <SearchBar submitHandler={searchBarHandler}></SearchBar>
                 <FlexSpace></FlexSpace>
                 {/* <div className="self-end">
                     {linksRight.map((x, idx) => <a key={idx} href={x[1]} className="mx-2">{x[0]}</a>)}
                 </div> */}
             </nav>
+            </div>
         </header>
     );
 }
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -9,6 +9,7 @@ export default function SearchBar(props: SearchBarProps) {
     return (
         <form action="" onSubmit={props.submitHandler}>
             <input type="text" name="search" placeholder={props.hint || ""} className="border-2 border-(--color-dark) rounded-md px-1" />
+            <button onClick={() => window.location.href='/search/' + encodeURIComponent((document.querySelector('input[name="search"]') as HTMLInputElement).value)} type="submit" className="ml-2 px-2 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300">Search</button> 
         </form>
     );
 }

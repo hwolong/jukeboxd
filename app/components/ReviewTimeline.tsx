b26320e938b08bd1a79e832ebd4cf182d72f08bf
addiebart <addiebart@ku.edu>
2025-11-09 02:49:09 -0600

    after pull

---
+++ app/components/ReviewTimeline.tsx
@@ -0,0 +1,12 @@
+export default async function ReviewTimeline() {
+    
+
+    return (
+        <div className="flex flex-row grow m-8">
+            <div className="grow"></div>
+            <section id="new_reviews" className="flex flex-row items-start w-9/10 md:w-4/5">
+            </section>
+            <div className="grow"></div>
+        </div>
+    ); 
+}
